8d1a351e45b31250188a3082e18f4093c103883d
imVituZeron <vitorpaulasantos@gmail.com>
2023-05-12 16:19:39 -0300

    initing createFile function and modularizing files

---
+++ models/people.go
@@ -0,0 +1,8 @@
+package models
+
+type Pessoa struct {
+	Id        int
+	Nome      string
+	Sobrenome string
+	Profissao string
+}
---
+++ utils/createFile.go
@@ -0,0 +1,12 @@
+package utils
+
+import (
+	"fmt"
+	P "project/packages/models"
+)
+
+func Createfile(value []P.Pessoa) string {
+	fmt.Println(value)
+
+	return "Finish!"
+}
